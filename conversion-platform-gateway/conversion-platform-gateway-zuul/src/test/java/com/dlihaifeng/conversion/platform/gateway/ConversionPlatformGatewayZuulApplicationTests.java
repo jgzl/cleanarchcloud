201adc70501eaf43a419bcfde943884353888fb6
李海峰 <li7hai26@gmail.com>
2019-11-24 00:49:35 +0800

    删除无用文件
    Signed-off-by: 李海峰 <li7hai26@gmail.com>

---
+++ conversion-platform-gateway/conversion-platform-gateway-zuul/src/test/java/com/dlihaifeng/conversion/platform/gateway/ConversionPlatformGatewayZuulApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dlihaifeng.conversion.platform.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionPlatformGatewayZuulApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
