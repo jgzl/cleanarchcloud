1cbea38e7e8b48dd89c53f4d4a5b9661cd9ded61
李海峰 <li7hai26@gmail.com>
2019-11-24 05:26:28 +0800

    将health所有信息完全打开
    Signed-off-by: 李海峰 <li7hai26@gmail.com>

--- conversion-platform-gateway/conversion-platform-gateway-sc/src/main/java/com/dlihaifeng/conversion/platform/gateway/controller/IndexController.java
+++ conversion-platform-gateway/conversion-platform-gateway-sc/src/main/java/com/dlihaifeng/conversion/platform/gateway/controller/IndexController.java
@@ -1,19 +1,23 @@
 package com.dlihaifeng.conversion.platform.gateway.controller;
 
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 /**
  * @author lihaifeng
  */
+@Slf4j
 @Controller
 @ResponseBody
 public class IndexController {
   @RequestMapping("ping")
-  public Mono<String> ping() {
+  public Mono<String> ping(ServerHttpRequest serverHttpRequest) {
+    log.info("serverRequest:{}", serverHttpRequest);
     return Mono.just("pong");
   }
 }
