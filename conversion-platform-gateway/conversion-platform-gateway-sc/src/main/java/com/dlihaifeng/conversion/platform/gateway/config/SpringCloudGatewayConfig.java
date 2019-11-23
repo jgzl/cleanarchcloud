b25ecd5bf3d9a20b4937b0ffb9237f6a3bfb26a1
李海峰 <li7hai26@gmail.com>
2019-11-24 07:08:09 +0800

    GlobalFilter,GatewayFilter,GatewayFilterFactory,RoutePredicateFactory
    Signed-off-by: 李海峰 <li7hai26@gmail.com>

---
+++ conversion-platform-gateway/conversion-platform-gateway-sc/src/main/java/com/dlihaifeng/conversion/platform/gateway/config/SpringCloudGatewayConfig.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2019-2019
+ */
+
+package com.dlihaifeng.conversion.platform.gateway.config;
+
+import org.springframework.cloud.gateway.handler.predicate.RoutePredicateFactory;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.dlihaifeng.conversion.platform.gateway.filter.CustomerGatewayFilter;
+import com.dlihaifeng.conversion.platform.gateway.filter.RequestGlobalFilter;
+import com.dlihaifeng.conversion.platform.gateway.predicate.DemoRoutePredicateFactory;
+
+/**
+ * @author lihaifeng
+ */
+@Configuration
+public class SpringCloudGatewayConfig {
+  @Bean
+  public RoutePredicateFactory routePredicateFactory() {
+    return new DemoRoutePredicateFactory();
+  }
+
+  @Bean
+  public RequestGlobalFilter requestGatewayFilter() {
+    return new RequestGlobalFilter();
+  }
+
+  @Bean
+  public CustomerGatewayFilter customerGatewayFilter() {
+    return new CustomerGatewayFilter();
+  }
+
+  @Bean
+  public RouteLocator customerRouteLocator(RouteLocatorBuilder builder) {
+    return builder.routes()
+        .route(r -> r.path("/customer/**")
+            .filters(f -> f.filter(new CustomerGatewayFilter())
+                .addResponseHeader("X-Response-test", "test"))
+            .uri("http://baidu.com:80")
+            .id("customer_filter_router")
+        )
+        .build();
+  }
+}
---
+++ conversion-platform-gateway/conversion-platform-gateway-sc/src/main/java/com/dlihaifeng/conversion/platform/gateway/filter/RequestGlobalFilter.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2019-2019
+ */
+
+package com.dlihaifeng.conversion.platform.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.server.ServerWebExchange;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author lihaifeng
+ */
+@Slf4j
+public class RequestGlobalFilter implements GlobalFilter, Ordered {
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+    ServerHttpRequest request = exchange.getRequest();
+    log.info("request:{}", request);
+    return chain.filter(exchange);
+  }
+
+  @Override
+  public int getOrder() {
+    return 0;
+  }
+}
