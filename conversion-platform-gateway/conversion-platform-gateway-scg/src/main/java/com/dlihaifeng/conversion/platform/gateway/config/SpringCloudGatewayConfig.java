6bb52e852580384cc7eb2da60529cc1684238349
李海峰 <li7hai26@gmail.com>
2019-11-24 16:45:50 +0800

    修复模块关系
    Signed-off-by: 李海峰 <li7hai26@gmail.com>

--- conversion-platform-gateway/conversion-platform-gateway-scg/src/main/java/com/dlihaifeng/conversion/platform/gateway/config/SpringCloudGatewayConfig.java
+++ conversion-platform-gateway/conversion-platform-gateway-scg/src/main/java/com/dlihaifeng/conversion/platform/gateway/config/SpringCloudGatewayConfig.java
@@ -1,52 +1,53 @@
 /*
  * Copyright (c) 2019-2019
- *//*
-
+ */
 
 package com.dlihaifeng.conversion.platform.gateway.config;
 
-import org.springframework.cloud.gateway.handler.predicate.RoutePredicateFactory;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
-import com.dlihaifeng.conversion.platform.gateway.filter.CustomerGatewayFilter;
-import com.dlihaifeng.conversion.platform.gateway.filter.RequestGlobalFilter;
-import com.dlihaifeng.conversion.platform.gateway.predicate.DemoRoutePredicateFactory;
-
-*/
 /**
  * @author lihaifeng
- *//*
-
+ */
 @Configuration
 public class SpringCloudGatewayConfig {
+  /*  @Bean
+    public RoutePredicateFactory routePredicateFactory() {
+      return new DemoRoutePredicateFactory();
+    }
+
+    @Bean
+    public RequestGlobalFilter requestGatewayFilter() {
+      return new RequestGlobalFilter();
+    }
+
+    @Bean
+    public CustomerGatewayFilter customerGatewayFilter() {
+      return new CustomerGatewayFilter();
+    }
+
+    @Bean
+    public RouteLocator customerRouteLocator(RouteLocatorBuilder builder) {
+      return builder.routes()
+          .route(r -> r.path("/customer/**")
+              .filters(f -> f.filter(new CustomerGatewayFilter())
+                  .addResponseHeader("X-Response-test", "test"))
+              .uri("http://httpbin.org:80/get")
+              .id("customer_filter_router")
+          )
+          .build();
+    }*/
+
+  /**
+   * ribbon负载均衡
+   * @return
+   */
   @Bean
-  public RoutePredicateFactory routePredicateFactory() {
-    return new DemoRoutePredicateFactory();
+  @LoadBalanced
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
   }
-
-  @Bean
-  public RequestGlobalFilter requestGatewayFilter() {
-    return new RequestGlobalFilter();
-  }
-
-  @Bean
-  public CustomerGatewayFilter customerGatewayFilter() {
-    return new CustomerGatewayFilter();
-  }
-
-*/
-/*  @Bean
-  public RouteLocator customerRouteLocator(RouteLocatorBuilder builder) {
-    return builder.routes()
-        .route(r -> r.path("/customer/**")
-            .filters(f -> f.filter(new CustomerGatewayFilter())
-                .addResponseHeader("X-Response-test", "test"))
-            .uri("http://httpbin.org:80/get")
-            .id("customer_filter_router")
-        )
-        .build();
-  }*//*
-
 }
-*/
