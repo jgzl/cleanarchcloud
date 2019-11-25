778af7687989d245c17b5697d34e045a929d1e56
李海峰 <li7hai26@gmail.com>
2019-11-25 20:05:25 +0800

    提交BootstrapUI版swagger-ui
    Signed-off-by: 李海峰 <li7hai26@gmail.com>

--- conversion-platform-gateway/conversion-platform-gateway-zuul/src/main/java/com/dlihaifeng/conversion/platform/gateway/ConversionPlatformGatewayZuulApplication.java
+++ conversion-platform-gateway/conversion-platform-gateway-zuul/src/main/java/com/dlihaifeng/conversion/platform/gateway/ConversionPlatformGatewayZuulApplication.java
@@ -6,10 +6,16 @@ package com.dlihaifeng.conversion.platform.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author lihaifeng
  */
+@EnableZuulProxy
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConversionPlatformGatewayZuulApplication {
 
