c7e945007d4143713de59a828c5e6b4186c96753
李海峰 <li7hai26@gmail.com>
2019-12-02 19:30:29 +0800

    增加freemarker静态模板配置
    Signed-off-by: 李海峰 <li7hai26@gmail.com>

---
+++ conversion-platform-application/conversion-platform-application-demo/src/main/java/com/dlihaifeng/conversion/platform/application/model/vo/WelcomeVO.java
@@ -0,0 +1,17 @@
+package com.dlihaifeng.conversion.platform.application.model.vo;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * @author lihaifeng
+ */
+@Data
+@Builder
+public class WelcomeVO {
+  private String userName;
+
+  private String urlName;
+
+  private String urlDescription;
+}
---
+++ conversion-platform-application/conversion-platform-application-demo/src/main/resources/templates/welcome.ftl
@@ -0,0 +1,12 @@
+<html>
+<head>
+  <title>欢迎页</title>
+</head>
+<body>
+<h1>Welcome to welcome.html!</h1>
+<h2>Welcome ${welcomeVO.userName}</h2>
+<div>
+  <a href="${welcomeVO.urlName}">${welcomeVO.urlDescription}</a>
+</div>
+</body>
+</html>
