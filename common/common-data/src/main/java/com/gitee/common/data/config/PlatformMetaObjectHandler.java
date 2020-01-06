6c304e6dad120308fe2395009f05753d3e018066
李海峰 <li7hai26@gmail.com>
2020-01-07 01:04:22 +0800

    自动填充数据优化
    Signed-off-by: 李海峰 <li7hai26@gmail.com>

--- common/common-data/src/main/java/com/gitee/common/data/config/PlatformMetaObjectHandler.java
+++ common/common-data/src/main/java/com/gitee/common/data/config/PlatformMetaObjectHandler.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.apache.ibatis.reflection.MetaObject;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.gitee.common.security.util.SecurityUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,16 +19,16 @@ public class PlatformMetaObjectHandler implements MetaObjectHandler {
   public void insertFill(MetaObject metaObject) {
     log.info("start insert fill ....");
     //版本号3.0.6以及之前的版本
-    this.setFieldValByName("createdBy", -1L, metaObject);
+    this.setFieldValByName("createdBy",SecurityUtils.getAuthentication()==null?-1L:SecurityUtils.getUser().getId(), metaObject);
     this.setFieldValByName("createdTime", LocalDateTime.now(), metaObject);
-    this.setFieldValByName("updatedBy", -1L, metaObject);
+    this.setFieldValByName("updatedBy", SecurityUtils.getAuthentication()==null?-1L:SecurityUtils.getUser().getId(), metaObject);
     this.setFieldValByName("updatedTime", LocalDateTime.now(), metaObject);
   }
 
   @Override
   public void updateFill(MetaObject metaObject) {
     log.info("start update fill ....");
-    this.setFieldValByName("updatedBy", -1L, metaObject);
+    this.setFieldValByName("updatedBy", SecurityUtils.getAuthentication()==null?-1L:SecurityUtils.getUser().getId(), metaObject);
     this.setFieldValByName("updatedTime", LocalDateTime.now(), metaObject);
   }
 }
