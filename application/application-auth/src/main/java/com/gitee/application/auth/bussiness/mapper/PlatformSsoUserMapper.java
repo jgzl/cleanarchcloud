6cb4a3e68ae791b206f7aae27e6a6ceb04e49066
李海峰 <li7hai26@gmail.com>
2020-01-07 20:59:51 +0800

    提交本地配置至nacos

--- application/application-auth/src/main/java/com/gitee/application/auth/bussiness/mapper/PlatformSsoUserMapper.java
+++ application/application-auth/src/main/java/com/gitee/application/auth/bussiness/mapper/PlatformSsoUserMapper.java
@@ -1,14 +1,11 @@
 package com.gitee.application.auth.bussiness.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
-
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.gitee.application.auth.bussiness.domain.PlatformSsoUser;
 
 /**
  * @author lihaifeng
  */
-@Mapper
 public interface PlatformSsoUserMapper extends BaseMapper<PlatformSsoUser> {
 
 }
--- common/common-data/src/main/java/com/gitee/common/data/config/PlatformMetaObjectHandler.java
+++ common/common-data/src/main/java/com/gitee/common/data/config/PlatformMetaObjectHandler.java
@@ -19,16 +19,16 @@ public class PlatformMetaObjectHandler implements MetaObjectHandler {
   public void insertFill(MetaObject metaObject) {
     log.info("start insert fill ....");
     //版本号3.0.6以及之前的版本
-    this.setFieldValByName("createdBy",SecurityUtils.getAuthentication()==null?-1L:SecurityUtils.getUser().getId(), metaObject);
+    this.setFieldValByName("createdBy",SecurityUtils.getAuthentication()==null?-1L:SecurityUtils.getUser()==null?-1L:SecurityUtils.getUser().getId(), metaObject);
     this.setFieldValByName("createdTime", LocalDateTime.now(), metaObject);
-    this.setFieldValByName("updatedBy", SecurityUtils.getAuthentication()==null?-1L:SecurityUtils.getUser().getId(), metaObject);
+    this.setFieldValByName("updatedBy", SecurityUtils.getAuthentication()==null?-1L:SecurityUtils.getUser()==null?-1L:SecurityUtils.getUser().getId(), metaObject);
     this.setFieldValByName("updatedTime", LocalDateTime.now(), metaObject);
   }
 
   @Override
   public void updateFill(MetaObject metaObject) {
     log.info("start update fill ....");
-    this.setFieldValByName("updatedBy", SecurityUtils.getAuthentication()==null?-1L:SecurityUtils.getUser().getId(), metaObject);
+    this.setFieldValByName("updatedBy", SecurityUtils.getAuthentication()==null?-1L:SecurityUtils.getUser()==null?-1L:SecurityUtils.getUser().getId(), metaObject);
     this.setFieldValByName("updatedTime", LocalDateTime.now(), metaObject);
   }
 }
