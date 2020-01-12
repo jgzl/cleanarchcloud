20c0e742f097f6f8357f4f0f1440d8bc7afe5a8d
李海峰 <li7hai26@gmail.com>
2020-01-12 21:02:54 +0800

    [FIX]更换接口导致UPMS无法启动

--- application/application-upms/src/main/java/com/gitee/application/upms/controller/PlatformSsoUserController.java
+++ application/application-upms/src/main/java/com/gitee/application/upms/controller/PlatformSsoUserController.java
@@ -1,17 +1,17 @@
 /*
- * Copyright [2020] [lihaifeng,xuhang]
+ *    Copyright [2020] [lihaifeng,xuhang]
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- *  limitations under the License.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package com.gitee.application.upms.controller;
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.gitee.application.upms.service.PlatformSsoUserService;
+import com.gitee.application.upms.service.IPlatformSsoUserService;
 import com.gitee.common.core.util.Result;
 import com.gitee.common.security.util.SecurityUtils;
 import com.gitee.common.upms.dto.UserDTO;
@@ -51,7 +51,7 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/user")
 @Api(value = "user", tags = "用户管理模块")
 public class PlatformSsoUserController {
-  private final PlatformSsoUserService userService;
+  private final IPlatformSsoUserService userService;
 
   /**
    * 获取当前用户全部信息
