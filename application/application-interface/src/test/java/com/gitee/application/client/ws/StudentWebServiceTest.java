e570dce3ce86dae8aeec67e45ef6fb34c58a9d9b
李海峰 <li7hai26@gmail.com>
2020-07-08 23:54:29 +0800

    refactor(接口模块): 增加相应的超时设置
    增加测试类相应的超时设置

--- application/application-interface/src/test/java/com/gitee/application/client/ws/StudentWebServiceTest.java
+++ application/application-interface/src/test/java/com/gitee/application/client/ws/StudentWebServiceTest.java
@@ -3,7 +3,11 @@ package com.gitee.application.client.ws;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxy;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
 
 import com.gitee.application.client.dto.StudentDTO;
 import com.gitee.application.client.dto.StudentRequestDTO;
@@ -21,12 +25,16 @@ public class StudentWebServiceTest {
 		jaxWsProxyFactoryBean.setServiceClass(StudentWebService.class);
 
 		StudentWebService studentWebService = (StudentWebService) jaxWsProxyFactoryBean.create(); // 创建客户端对象
-//		Client proxy= ClientProxy.getClient(studentWebService);
-//		HTTPConduit conduit=(HTTPConduit)proxy.getConduit();
-//		HTTPClientPolicy policy=new HTTPClientPolicy();
-//		policy.setConnectionTimeout(1000);
-//		policy.setReceiveTimeout(1000);
-//		conduit.setClient(policy);
+
+		/**
+		 * 提供相应的超时设置
+		 */
+		Client proxy= ClientProxy.getClient(studentWebService);
+		HTTPConduit conduit=(HTTPConduit)proxy.getConduit();
+		HTTPClientPolicy policy=new HTTPClientPolicy();
+		policy.setConnectionTimeout(10000);
+		policy.setReceiveTimeout(10000);
+		conduit.setClient(policy);
 
 		WebServiceHeaderDTO webServiceHeaderDTO = new WebServiceHeaderDTO();
 		WebServiceBodyDTO<StudentDTO> webServiceBodyDTO = new WebServiceBodyDTO<>();
