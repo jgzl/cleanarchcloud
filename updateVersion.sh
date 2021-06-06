6d5924cea47ce7d78796907cec0ec1e44d1e016e
lihaifeng <li7hai26@gmail.com>
2021-06-06 21:21:37 +0800

    修改占位符revison为revision

--- updateVersion.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-workdir=$(cd $(dirname $0); pwd)
-echo '工作目录:' $workdir
-cd $workdir/cleanarch-core/cleanarch-starter-dependencies && mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$1
-cd $workdir/cleanarch-core && mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$1
-cd $workdir && mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$1
-mvn clean $2
