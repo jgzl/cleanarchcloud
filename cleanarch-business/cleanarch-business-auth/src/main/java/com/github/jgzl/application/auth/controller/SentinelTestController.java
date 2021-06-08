4d1d29f797acb801d48da3d0a4c09ac18fb0bb4c
lihaifeng <li7hai26@gmail.com>
2021-06-09 01:41:11 +0800

    refactor 重构代码，增加swagger注解

--- cleanarch-business/cleanarch-business-auth/src/main/java/com/github/jgzl/application/auth/controller/SentinelTestController.java
+++ cleanarch-business/cleanarch-business-auth/src/main/java/com/github/jgzl/application/auth/controller/SentinelTestController.java
@@ -1,5 +1,6 @@
 package com.github.jgzl.application.auth.controller;
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * 测试Sentinel
  * @author lihaifeng
  */
+@Api(tags = "Sentinel测试控制器")
 @RestController
 @RequestMapping("test")
 public class SentinelTestController {
--- cleanarch-business/cleanarch-business-auth/src/main/java/com/github/jgzl/application/auth/controller/UserController.java
+++ cleanarch-business/cleanarch-business-auth/src/main/java/com/github/jgzl/application/auth/controller/UserController.java
@@ -1,33 +1,25 @@
 package com.github.jgzl.application.auth.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-import javax.validation.constraints.Pattern;
-
+import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.github.jgzl.application.auth.service.SysUserService;
 import com.github.jgzl.common.core.constant.CacheConstants;
 import com.github.jgzl.common.core.util.Result;
 import com.github.jgzl.common.data.redis.CustomRedisRepository;
 import com.github.jgzl.common.security.validate.Add;
 import com.github.jgzl.common.security.vo.SysUserVO;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-
-import cn.hutool.core.util.RandomUtil;
-import lombok.extern.slf4j.Slf4j;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
 
 /**
  * 用户Controller
@@ -35,6 +27,7 @@ import lombok.extern.slf4j.Slf4j;
  * @author lihaifeng
  * 2019/7/4 15:44
  */
+@Api(tags = "用户控制器")
 @Slf4j
 @Controller
 @Validated
@@ -55,7 +48,7 @@ public class UserController {
 	 * @param authentication 信息
 	 * @return 用户信息
 	 */
-	@RequestMapping({"/userInfo", "/info"})
+	@GetMapping("/info")
 	@ResponseBody
 	public ResponseEntity<Result> userInfo(Authentication authentication) {
 		if (authentication == null) {
@@ -70,10 +63,9 @@ public class UserController {
 	 * @param mobile
 	 * @return
 	 */
-	@RequestMapping("/smsCode/{mobile}")
+	@GetMapping("/smsCode/{mobile}")
 	@ResponseBody
-	public ResponseEntity<Result> smsCode(@Pattern(regexp = MOBILE_REG, message = "请输入正确的手机号")
-	@PathVariable String mobile) {
+	public ResponseEntity<Result> smsCode(@Pattern(regexp = MOBILE_REG, message = "请输入正确的手机号") @PathVariable String mobile) {
 		Object tempCode = redisRepository.get(CacheConstants.DEFAULT_CODE_KEY + mobile);
 		if (tempCode != null) {
 			log.error("用户:{}验证码未失效{}", mobile, tempCode);
