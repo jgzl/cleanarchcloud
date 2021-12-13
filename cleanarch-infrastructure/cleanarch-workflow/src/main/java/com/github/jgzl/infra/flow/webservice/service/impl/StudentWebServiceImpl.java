109d65906d385418a49a8c6c5d34260a40cbff0b
架构之路在何方 <li7hai26@outlook.com>
2021-12-13 21:24:18 +0800

    增加环境区分profiles,结合maven profiles与spring boot profiles,打包动态配置化

--- cleanarch-infrastructure/cleanarch-workflow/src/main/java/com/github/jgzl/infra/flow/webservice/service/impl/StudentWebServiceImpl.java
+++ cleanarch-infrastructure/cleanarch-workflow/src/main/java/com/github/jgzl/infra/flow/webservice/service/impl/StudentWebServiceImpl.java
@@ -1,14 +1,22 @@
 package com.github.jgzl.infra.flow.webservice.service.impl;
-import java.util.List;
-import javax.jws.WebService;
 
 import com.github.jgzl.infra.flow.webservice.common.WebServiceConstants;
-import com.github.jgzl.infra.flow.webservice.dto.StudentDTO;
-import com.github.jgzl.infra.flow.webservice.dto.StudentRequestDTO;
-import com.github.jgzl.infra.flow.webservice.dto.StudentResponseDTO;
+import com.github.jgzl.infra.flow.webservice.dto.*;
 import com.github.jgzl.infra.flow.webservice.service.StudentWebService;
 import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
 
+import javax.jws.WebService;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
 @WebService(targetNamespace = WebServiceConstants.CAMUNDA_NAMESPACE)
 public class StudentWebServiceImpl implements StudentWebService<StudentDTO> {
 	@Override
@@ -28,5 +36,35 @@ public class StudentWebServiceImpl implements StudentWebService<StudentDTO> {
 		}
 		responseDTO.setStudents(students);
 		return responseDTO;
+	}
+
+	public static void main(String[] args) {
+		JaxWsProxyFactoryBean jaxWsProxyFactoryBean=new JaxWsProxyFactoryBean();
+		jaxWsProxyFactoryBean.setAddress("http://127.0.0.1:8101/services/student?wsdl");
+		jaxWsProxyFactoryBean.setServiceClass(StudentWebService.class);
+
+		StudentWebService studentWebService = (StudentWebService) jaxWsProxyFactoryBean.create(); // 创建客户端对象
+		Client proxy= ClientProxy.getClient(studentWebService);
+		HTTPConduit conduit=(HTTPConduit)proxy.getConduit();
+		HTTPClientPolicy policy=new HTTPClientPolicy();
+		policy.setConnectionTimeout(1000);
+		policy.setReceiveTimeout(1000);
+		conduit.setClient(policy);
+
+		WebServiceHeaderDTO webServiceHeaderDTO = new WebServiceHeaderDTO();
+		WebServiceBodyDTO<StudentDTO> webServiceBodyDTO = new WebServiceBodyDTO<>();
+		StudentRequestDTO<StudentDTO> studentRequestDTO = new StudentRequestDTO<>();
+		StudentDTO student = new StudentDTO();
+		webServiceHeaderDTO.setRequestId(UUID.randomUUID().toString());
+		webServiceHeaderDTO.setServiceName("student");
+		student.setUsername("lihaifeng");
+		student.setNickname("lihaifeng");
+		webServiceBodyDTO.setData(student);
+		studentRequestDTO.setServiceHeader(webServiceHeaderDTO);
+		studentRequestDTO.setServiceBody(webServiceBodyDTO);
+		StudentResponseDTO studentResponseDTO= studentWebService.fetchStudent(studentRequestDTO);
+		Optional.ofNullable(studentResponseDTO.getStudents()).ifPresent(studentDTO -> {
+			log.info("studentDTO:{}",studentDTO);
+		});
 	}
 }
--- cleanarch-infrastructure/cleanarch-workflow/src/test/java/com/github/jgzl/infra/flow/webservice/LogTest.java
+++ cleanarch-infrastructure/cleanarch-workflow/src/test/java/com/github/jgzl/infra/flow/webservice/LogTest.java
@@ -1,14 +1,11 @@
 package com.github.jgzl.infra.flow.webservice;
+
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@Slf4j
 public class LogTest {
-
-	private static final Logger log = LoggerFactory.getLogger(LogTest.class);
 
 	@Test
 	public void logTest(){
@@ -19,4 +16,5 @@ public class LogTest {
 		MDC.put("demoKey2","demoKeyValue22");
 		log.info("测试MDC 2设置值");
 	}
+
 }
